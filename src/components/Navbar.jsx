9eacfbfc7cac266e77c4f9904528d63bbd620410
some19ice <some1me247@gmail.com>
2021-09-30 04:49:25 +0100

    added Homepage, exchanges, news, cryptocurrencies, and crypto details components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,11 @@ const Navbar = () => {
   return (
     <div className="nav-container" >
       <div className="logo-container">
-        <Avatar src={icon} size="small"/>
+        <Avatar src={icon} size="large" />
         <Typography.Title level={2} className="logo">
           <Link to="/">Crypto App</Link>
         </Typography.Title>
+      </div>
         <Menu theme='dark'>
           <Menu.Item icon={<HomeOutlined />}>
             <Link to="/">Home</Link>
@@ -27,7 +28,7 @@ const Navbar = () => {
             <Link to="/news">News</Link>
           </Menu.Item>
         </Menu>
-      </div>
+      
     </div>
   )
 }
